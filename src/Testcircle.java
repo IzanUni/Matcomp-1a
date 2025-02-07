a9d1f7f4ea69584c19fdefcb8a7e2035aff366e0
IzanUni <izan.cubas@edu.uah.es>
2025-02-07 09:19:14 +0100

    Clases de la inttroducción de la 1 práctica

---
+++ src/Testcircle.java
@@ -0,0 +1,23 @@
+public class Testcircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+
+        System.out.println("The circle has radius of "
+                + c1.getRadius() + " and area of " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+
+        System.out.println("The circle has radius of "
+                + c2.getRadius() + " and area of " + c2.getArea());
+
+        Circle c3 = new Circle(3.0, "Azul");
+
+        System.out.println("The circle has radius of " + c3.getRadius() + " and area of " + c3.getArea());
+        System.out.println("The color of the circle is " + c3.getColor());
+        Circle c4 = new Circle(); // construct an instance of Circle
+        c4.setRadius(5.5); // change radius
+        System.out.println("radius is: " + c4.getRadius()); // Print radius via getter
+        c4.setColor("green"); // Change color
+        System.out.println("color is: " + c4.getColor()); // Print color via getter
+    }
+}
